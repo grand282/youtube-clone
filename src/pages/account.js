cf2360c6edbd0c1a565b25e13937a754919f571d
grand282 <115876301+grand282@users.noreply.github.com>
2023-06-06 23:30:07 +0300

    Update account.js

--- src/pages/account.js
+++ src/pages/account.js
@@ -18,7 +18,7 @@ const Account = () => {
 
 
         <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:ring-2 focus:ring-blue-500" 
-        onClick={()=> signOut({ callbackUrl: 'http://localhost:3000/' })}>signOut</button>
+        onClick={()=> signOut({ callbackUrl: '/' })}>signOut</button>
     </div>
     </>
     )}
